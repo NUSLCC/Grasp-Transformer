6fea2cd9cfce8ce9d709ddc4f0941dad7036b672
Chenchen Liu <chenchen.liu@u.nus.edu>
2024-07-18 19:51:51 +0800

    comma

--- main_k_fold.py
+++ main_k_fold.py
@@ -24,7 +24,7 @@ def parse_args():
     # Dataset & Data & Training
     home_dir = os.path.expanduser("~")
     parser.add_argument('--dataset', type=str,default="cornell", help='Dataset Name ("cornell" or "jaquard or multi")')
-    parser.add_argument('--dataset-path', type=str,default=home_dir + "/Dataset/cornell" ,help='Path to dataset')
+    parser.add_argument('--dataset-path', type=str,default=home_dir + "/Dataset/cornell", help='Path to dataset')
     parser.add_argument('--use-depth', type=int, default=1, help='Use Depth image for training (1/0)')
     parser.add_argument('--use-rgb', type=int, default=0, help='Use RGB image for training (0/1)')
     parser.add_argument('--split', type=float, default=0.9, help='Fraction of data for training (remainder is validation)')
